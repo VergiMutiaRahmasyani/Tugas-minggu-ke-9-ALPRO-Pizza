ea5c497b52a2e8a76b9e4d607352b9208386819f
VergiMutiaRahmasyani <147597045+VergiMutiaRahmasyani@users.noreply.github.com>
2023-11-01 17:26:14 +0700

    Create Tugas Minggu 9 Pizza Kel.5 2023E.py

---
+++ Tugas Minggu 9 Pizza Kel.5 2023E.py
@@ -0,0 +1,10 @@
+"""
+    Kelas    : 2023E
+    Prodi    : D4 Manajemen Informatika
+    Kelompok : 5
+
+    Nama Anggota :
+    1. Achmad Syamsudin       - 23091397164
+    2. Ahmad Aryobimo         - 23091397151
+    3. Vergi Mutia Rahmasyani - 23091397171
+"""
